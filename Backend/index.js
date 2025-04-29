65bb012bd23b44601155dbda5065ca626350b6df
RenFebriansyah <154679554+RenFebriansyah@users.noreply.github.com>
2025-04-29 18:48:01 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,11 @@ import cors from "cors";
 import NoteRoute from "./routes/NoteRoutes.js";
 
 const app = express();
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.render("index"));
 app.use(NoteRoute);
 
 app.listen(4000, ()=> console.log('Server aktif dan berjalan...'));
