874ff31541749116eb5930fe528edd3132004aab
RenFebriansyah <154679554+RenFebriansyah@users.noreply.github.com>
2025-04-30 00:47:09 +0700

    Update database.js

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,7 +3,7 @@ import {Sequelize} from "sequelize";
 const db = new Sequelize('Note_Final','root','123220033',{
     host: '35.222.111.92',
     dialect: 'mysql',
-    port: 3306
+   
 });
 
 export default db;
