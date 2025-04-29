cb172bceeff6eaa3146804834b2b5a099fb1a529
Wahyu Febriansyah <123220033@student.upnyk.ac.id>
2025-04-29 17:12:49 +0700

    Initial commit

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,8 +1,9 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('note_final','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize('Note_Final','rensql','123220033',{
+    host: '35.222.111.92',
+    dialect: 'mysql',
+    port: 3306
 });
 
 export default db;
